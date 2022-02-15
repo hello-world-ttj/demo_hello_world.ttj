4050322761524281f88e1fd5a6c02be4f9138ceb
Tijo T Joseph <tijotjoseph003@gmail.com>
2022-02-15 23:37:39 +0530

    Created basic structure for my profile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import AboutMe from './AboutMe';
+import Contact from './Contact';
+import Projects from './Projects';
+import Resume from './Resume';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className='Navbar'>
+          <nav className='Nav'>
+            <p className="Title"><span className='Symbol'>&#9883;</span> Tijo T Joseph <span className='Sub-title'>/ web developer</span></p> 
+            <ul>
+              <li>
+                <Link to="/contact" className='link'>contact</Link>
+              </li>
+              <li>
+                <Link to="/projects" className='link'>projects</Link>
+              </li>
+              <li>
+                <Link to="/resume" className='link'>resume</Link>
+              </li>
+              <li>
+                <Link exact to="/" className='link'>About Me</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/" element={<AboutMe />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
