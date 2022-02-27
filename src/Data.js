7a066598dfa0f16844712f16a278dfb53a1fbef6
Tijo T Joseph <tijotjoseph003@gmail.com>
2022-02-27 23:03:47 +0530

    Project added

---
+++ src/Data.js
@@ -0,0 +1,68 @@
+let Data = [
+    {
+        id: "001",
+        title: "Memes Generator",
+        img: "images/Meme.jpeg",
+        lang: "React",
+        des: "We can create meme through this web app",
+        link: "https://hello-world-ttj-meme-generator.netlify.app/"
+    },
+    {
+        id: "002",
+        title: "ToDo",
+        img: "images/Todo.jpeg",
+        lang: "React",
+        des: "Its a todo app",
+        link: "https://hello-world-ttj-todo-app.netlify.app/"
+    },
+    {
+        id: "003",
+        title: "Black Jack",
+        img: "images/black.jpeg",
+        lang: "JS",
+        des: "Black Jack Game",
+        link: "https://hello-world-ttj.github.io/blackJack/"
+    },
+    {
+        id: "004",
+        title: "Rock Paper Scissors",
+        img: "images/Rps.jpeg",
+        lang: "JS",
+        des: "Rock Paper Scissors Game",
+        link: "https://hello-world-ttj.github.io/rock-paper-scissors/"
+    },
+    {
+        id: "005",
+        title: "Kurup",
+        img: "images/kurup.jpeg",
+        lang: "React",
+        des: "Kurup movie details",
+        link: "https://kurup.netlify.app/"
+    },
+    {
+        id: "006",
+        title: "Counter",
+        img: "images/counter.jpeg",
+        lang: "JS",
+        des: "Counter App",
+        link: "https://hello-world-ttj.github.io/js_counter/"
+    },
+    {
+        id: "007",
+        title: "Apple Clone",
+        img: "images/apple.png",
+        lang: "Node JS, Handlebars & MongoDB",
+        des: "BCA Main Project",
+        link: ""
+    },
+    {
+        id: "008",
+        title: "Educa",
+        img: "images/educa.jpeg",
+        lang: "PHP & MYSQL",
+        des: "BCA Mini Project",
+        link: ""
+    }
+]
+
+export default Data
--- src/Projects.js
+++ src/Projects.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import Footer from './Footer'
+import './Projects.scss'
+import Data from './Data'
 
 function Projects() {
+
+  const projects = Data.map(data => {
+    return (
+      <div className="card">
+        <div className="content">
+          <div className="col--name">
+          <div className="color"></div>
+          <h2 className="name">{data.title}</h2>
+          </div>
+          <p className="language">{data.lang}</p>
+          <p className="desc">{data.des}</p>
+        {data.link && <a href={data.link} className='view'><img alt='icon' src="https://img.icons8.com/emoji/50/000000/eye-in-speech-bubble.png"/> View</a>} 
+        </div>
+        <div className="image">
+        <img src={data.img} alt="site img" />
+        </div>
+      </div>
+    )
+  })
+
+
   return (
-    <div>Projects</div>
+    <div className='project'>
+      <div className="project--container">
+        <h1> <span className='symbol'>&#10070;</span> Projects</h1>
+        <div className="cards">
+        {projects}
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
